cf514ff392885e379291abe7306ea4fd04238aff
“Sten <sten.uuesoo@gmail.com>
2023-09-01 17:06:07 +0300

    added alot of improvemnts in code style and functionalities.

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
 import json
+import logging
+from typing import Any, Dict, List, Optional, Union
 
-rawResult = "{\"service\":\"Private person\",\"version\":1,\"requestID\":1693392409,\"result\":{\"endpoint\":\"RR437\",\"response\":{\"r:report\":{\"prod:RR437Response\":{\"request\":{\"Isikukood\":\"49310052510\"},\"response\":{\"Isik\":{\"Isik.Isikukood\":\"49310052510\",\"Isik.Eesnimi\":\"IRINA\",\"Isik.Perenimi\":\"LOSSEVA\",\"Isik.Sugu\":\"NAINE\",\"Isik.Synniaeg\":\"1993-10-05\",\"Isik.IsikuStaatus\":\"E\",\"Isik.KirjeStaatus\":\"K\",\"Isik.Kodakondsus\":\"0\",\"Elukoht\":{\"Elukoht.RiigiKood\":\"EST\",\"Elukoht.RiigiNimi\":\"Eesti\",\"Elukoht.MaakonnaKood\":\"45\",\"Elukoht.MaakonnaNimi\":\"Ida-Viru maakond\",\"Elukoht.VallaKood\":\"321\",\"Elukoht.VallaNimi\":\"Kohtla-Järve linn\",\"Elukoht.AsulaKood\":\"120\",\"Elukoht.AsulaNimi\":\"Ahtme linnaosa\",\"Elukoht.TanavaNimi\":\"Puru tee\",\"Elukoht.Majanr\":\"80\",\"Elukoht.Korternr\":\"6\",\"Elukoht.Postiindeks\":\"31024\",\"Elukoht.Alates\":\"2022-07-01\",\"Elukoht.AadressTekstina\":\"Eesti, Ida-Viru maakond, Kohtla-Järve linn, Ahtme linnaosa, Puru tee 80-6\",\"Elukoht.AdsAdrID\":\"2769629\",\"Elukoht.AdsOid\":\"ER01395247\"},\"ElamisDokumendiTunnus\":\"2\",\"ElamisDokument\":\"ELAMISÕIGUS\",\"LasteArv\":\"3\",\"Dokumendid\":{\"Dokument\":[{\"Dokument.Liik\":\"ELAMISLUBA\",\"Dokument.Number\":\"TE334973847\",\"Dokument.ValjaAntud\":\"1995-10-13\",\"Dokument.KehtibKuni\":\"2000-10-14\",\"Dokument.Staatus\":\"KEHTETU\",\"Dokument.Asutus\":\"KODAKONDSUS- JA MIGRATSIOONIOSAKOND - PPA\"},{\"Dokument.Liik\":\"ELAMISLUBA\",\"Dokument.Number\":\"EL33D992970\",\"Dokument.ValjaAntud\":\"1999-10-06\",\"Dokument.KehtibKuni\":\"2014-02-10\",\"Dokument.Staatus\":\"KEHTETU\",\"Dokument.Asutus\":\"KODAKONDSUS- JA MIGRATSIOONIOSAKOND - PPA\"},{\"Dokument.Liik\":\"VÄLISMAALASE PASS\",\"Dokument.Number\":\"V0439089\",\"Dokument.ValjaAntud\":\"2003-10-28\",\"Dokument.KehtibKuni\":\"2008-08-22\",\"Dokument.Staatus\":\"KEHTETU\",\"Dokument.Asutus\":\"KODAKONDSUS- JA MIGRATSIOONIOSAKOND - PPA\"},{\"Dokument.Liik\":\"ISIKUTUNNISTUS\",\"Dokument.Number\":\"B0045726\",\"Dokument.ValjaAntud\":\"2003-10-28\",\"Dokument.KehtibKuni\":\"2008-08-22\",\"Dokument.Staatus\":\"KEHTETU\",\"Dokument.Asutus\":\"KODAKONDSUS- JA MIGRATSIOONIOSAKOND - PPA\"},{\"Dokument.Liik\":\"VÄLISMAALASE PASS\",\"Dokument.Number\":\"VB0004964\",\"Dokument.ValjaAntud\":\"2008-07-24\",\"Dokument.KehtibKuni\":\"2013-07-25\",\"Dokument.Staatus\":\"KEHTETU\",\"Dokument.Asutus\":\"KODAKONDSUS- JA MIGRATSIOONIOSAKOND - PPA\"},{\"Dokument.Liik\":\"ISIKUTUNNISTUS\",\"Dokument.Number\":\"P0072707\",\"Dokument.ValjaAntud\":\"2008-07-24\",\"Dokument.KehtibKuni\":\"2013-07-25\",\"Dokument.Staatus\":\"KEHTETU\",\"Dokument.Asutus\":\"KODAKONDSUS- JA MIGRATSIOONIOSAKOND - PPA\"},{\"Dokument.Liik\":\"ELAMISÕIGUS\",\"Dokument.Number\":\"1027595982/AO\",\"Dokument.ValjaAntud\":\"2014-02-20\",\"Dokument.Staatus\":\"KEHTIV\",\"Dokument.Asutus\":\"POLITSEI- JA PIIRIVALVEAMET\"},{\"Dokument.Liik\":\"VÄLISRIIGI PASS\",\"Dokument.Number\":\"PE2003293\",\"Dokument.ValjaAntud\":\"2013-06-07\",\"Dokument.KehtibKuni\":\"2023-06-06\",\"Dokument.Staatus\":\"KEHTETU\",\"Dokument.Asutus\":\"IIRIMAA  ASUTUSED\"},{\"Dokument.Liik\":\"ISIKUTUNNISTUS\",\"Dokument.Number\":\"EA0029396\",\"Dokument.ValjaAntud\":\"2014-02-20\",\"Dokument.KehtibKuni\":\"2015-09-25\",\"Dokument.Staatus\":\"KEHTETU\",\"Dokument.Asutus\":\"POLITSEI- JA PIIRIVALVEAMET\"},{\"Dokument.Liik\":\"ISIKUTUNNISTUS\",\"Dokument.Number\":\"EA0044330\",\"Dokument.ValjaAntud\":\"2015-09-11\",\"Dokument.KehtibKuni\":\"2020-09-12\",\"Dokument.Staatus\":\"KEHTETU\",\"Dokument.Asutus\":\"POLITSEI- JA PIIRIVALVEAMET\"},{\"Dokument.Liik\":\"JUHILUBA\",\"Dokument.Number\":\"EV584822\",\"Dokument.ValjaAntud\":\"2020-09-08\",\"Dokument.KehtibKuni\":\"2030-08-20\",\"Dokument.Staatus\":\"KEHTIV\",\"Dokument.Asutus\":\"TRANSPORDIAMET\"},{\"Dokument.Liik\":\"ISIKUTUNNISTUS\",\"Dokument.Number\":\"EB0520704\",\"Dokument.ValjaAntud\":\"2020-09-01\",\"Dokument.KehtibKuni\":\"2025-09-02\",\"Dokument.Staatus\":\"KEHTIV\",\"Dokument.Asutus\":\"POLITSEI- JA PIIRIVALVEAMET\"}]},\"AktiivsedOigused\":null,\"Sallivused\":null,\"Kontakt\":null,\"KeeludJaKasitlused\":null,\"Karistusregister\":null}}}}}}}"
-parsedResult = json.loads(rawResult)
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+KeyType = Union[str, int]
+NestedKeys = List[KeyType]
+
+# Define constants for JSON keys
+RESULT = "result"
+RESPONSE = "response"
+REPORT = "r:report"
+RR437_RESPONSE = "prod:RR437Response"
+ISIK = "Isik"
+DOCUMENTS = "Dokumendid"
+DOCUMENT = "Dokument"
+DOCUMENT_VALIDITY = "KEHTIV"
+DOCUMENT_TYPES = ["ELAMISLUBA", "ELAMISÕIGUS", "ELAMISLOAKAART", "PASS", "ISIKUTUNNISTUS"]
 
 
-def get_nested_key(data, keys):
+# Read and parse a JSON file
+def read_file(file_path: str) -> Optional[Dict[str, Any]]:
+    try:
+        with open(file_path, "r") as file:
+            raw_result = file.read().strip()
+    except FileNotFoundError:
+        logging.error(f"File {file_path} not found.")
+        return None
+
+    corrected_result = raw_result.replace(r'\"', '"')
+    try:
+        return json.loads(corrected_result)
+    except json.JSONDecodeError as json_error:
+        logging.error(f"An error occurred while parsing the JSON: {json_error}")
+        return None
+
+
+# Retrieve nested key values from a dictionary
+def get_nested_key(data: Dict[str, Any], keys: List[str]) -> Optional[Any]:
     for key in keys:
         if key in data:
             data = data[key]
@@ -13,29 +47,106 @@ def get_nested_key(data, keys):
     return data
 
 
-KEHTIV = "KEHTIV"
-DOCUMENTS_OF_INTEREST = ["ELAMISLUBA", "ELAMISÕIGUS", "ELAMISLOAKAART", "PASS", "ISIKUTUNNISTUS"]
+def extract_documents(parsed_result: Dict[str, Any]) -> Optional[List[Dict[str, Any]]]:
+    """Extract document details from parsed JSON result. Retrieve nested key values from a dictionary."""
+    return get_nested_key(
+        parsed_result,
+        [RESULT, RESPONSE, REPORT, RR437_RESPONSE, RESPONSE, ISIK, DOCUMENTS, DOCUMENT]
+    )
 
-first_name = get_nested_key(parsedResult, ["result", "response", "r:report", "prod:RR437Response", "response", "Isik",
-                                           "Isik.Eesnimi"])
-last_name = get_nested_key(parsedResult, ["result", "response", "r:report", "prod:RR437Response", "response", "Isik",
-                                          "Isik.Perenimi"])
 
-documents = get_nested_key(parsedResult,
-                           ["result", "response", "r:report", "prod:RR437Response", "response", "Isik", "Dokumendid",
-                            "Dokument"])
+def extract_multiple_keys(data: Dict[str, Any], key_paths: List[List[str]]) -> Dict[str, Optional[Any]]:
+    """Extract multiple nested keys from a dictionary and return a dictionary of results"""
+    results = {}
+    for keys in key_paths:
+        temp_data = data
+        for key in keys:
+            temp_data = temp_data.get(key, {})
+        key_str = '.'.join(keys)
+        results[key_str] = temp_data if temp_data else None
+    return results
 
-print(f"{first_name} {last_name}:")
 
-for doc in documents:
-    doc_type = doc.get("Dokument.Liik")
-    if any(interest in doc_type for interest in DOCUMENTS_OF_INTEREST) and doc.get("Dokument.Staatus") == KEHTIV:
+def fetch_data_from_keys(json_result: Dict[str, Any]) -> Dict[str, Optional[Any]]:
+    """Fetch and return a dictionary of values corresponding to given nested keys."""
+    key_paths: List[NestedKeys] = [
+        [RESULT, RESPONSE, REPORT, RR437_RESPONSE, RESPONSE, ISIK, "Isik.Eesnimi"],
+        [RESULT, RESPONSE, REPORT, RR437_RESPONSE, RESPONSE, ISIK, "Isik.Perenimi"],
+        [RESULT, RESPONSE, REPORT, RR437_RESPONSE, RESPONSE, ISIK, DOCUMENTS, DOCUMENT]
+    ]
+    return extract_multiple_keys(json_result, key_paths)
+
+
+def assemble_name_details(keys_result: Dict[str, Optional[Any]]) -> str:
+    """Assemble and return the name details from the results."""
+    first_name_key = '.'.join([RESULT, RESPONSE, REPORT, RR437_RESPONSE, RESPONSE, ISIK, "Isik.Eesnimi"])
+    last_name_key = '.'.join([RESULT, RESPONSE, REPORT, RR437_RESPONSE, RESPONSE, ISIK, "Isik.Perenimi"])
+    first_name = keys_result.get(first_name_key)
+    last_name = keys_result.get(last_name_key)
+    return f"{first_name or 'Unknown'} {last_name or 'Unknown'}"
+
+
+def filter_documents(documents: List[Dict[str, Any]], doc_types: List[str], status: str) -> List[Dict[str, Any]]:
+    """Filter documents based on types and status."""
+    return [
+        doc for doc in documents
+        if doc.get("Dokument.Liik") in doc_types and doc.get("Dokument.Staatus") == status
+    ]
+
+
+def log_and_prepare_results(filtered_docs: List[Dict[str, Any]]) -> List[str]:
+    """Prepare and log document details."""
+    results = []
+    for doc in filtered_docs:
+        doc_type = doc["Dokument.Liik"]
         doc_number = doc["Dokument.Number"]
         doc_status = doc["Dokument.Staatus"]
-        doc_va = doc["Dokument.ValjaAntud"]
-
+        result_line = f"{doc_type} {doc_status} nr: {doc_number}"
         if "Dokument.KehtibKuni" in doc:
-            doc_kk = doc["Dokument.KehtibKuni"]
-            print(f"{doc_type} {doc_status} nr: {doc_number} kehtib kuni {doc_kk}")
-        else:
-            print(f"{doc_type} {doc_status} nr: {doc_number}")
+            result_line += f" kehtib kuni {doc['Dokument.KehtibKuni']}"
+        logging.info(result_line)
+        results.append(result_line)
+    return results
+
+
+def filter_and_log_documents(keys_result: Dict[str, Optional[Any]], search_criteria: Dict[str, str]) -> List[str]:
+    """Filter, log, and prepare documents."""
+    documents_entries_key = '.'.join([RESULT, RESPONSE, REPORT, RR437_RESPONSE, RESPONSE, ISIK, DOCUMENTS, DOCUMENT])
+    documents_entries = keys_result.get(documents_entries_key)
+    if documents_entries is None:
+        logging.error("No documents found.")
+        return []
+    filtered_docs = filter_documents(documents_entries, search_criteria["types"], search_criteria["status"])
+    return log_and_prepare_results(filtered_docs)
+
+
+def save_to_file(name_details: str, results: List[str], file_name: str = "result.txt") -> None:
+    """Save results to a file."""
+    with open(file_name, "w") as file:
+        file.write(f"{name_details}\n")
+        for result in results:
+            file.write(result + "\n")
+
+
+def main() -> None:
+    """Main function to orchestrate the operations."""
+    json_result = read_file("data.txt")
+    if json_result is None:
+        return
+
+    search_criteria: Dict[str, Union[str, List[str]]] = {
+        "status": DOCUMENT_VALIDITY,
+        "types": DOCUMENT_TYPES
+    }
+
+    keys_result = fetch_data_from_keys(json_result)
+    name_details = assemble_name_details(keys_result)
+    logging.info(name_details)
+
+    results = filter_and_log_documents(keys_result, search_criteria)
+    if results:
+        save_to_file(name_details, results)
+
+
+if __name__ == "__main__":
+    main()
